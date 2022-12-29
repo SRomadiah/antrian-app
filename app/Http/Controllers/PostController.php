52955f4f28472368d96644bf5e1e07b27bbcc052
SRomadiah <diyah1888@gmail.com>
2022-12-29 21:27:44 +0800

    next

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::latest()->get();
+        return view('posts.index', compact('posts'));
+    }
+    public function create()
+    {
+        return view('posts.create');
+    }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required|string|max:155',
+            'content' => 'required',
+            'status' => 'required'
+        ]);
+
+        $post = Post::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'status' => $request->status,
+            'slug' => Str::slug($request->title)
+        ]);
+
+        if ($post) {
+            return redirect()
+                ->route('post.index')
+                ->with([
+                    'success' => 'New post has been created successfully'
+                ]);
+        } else {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with([
+                    'error' => 'Some problem occurred, please try again'
+                ]);
+        }
+    }
+
+}
